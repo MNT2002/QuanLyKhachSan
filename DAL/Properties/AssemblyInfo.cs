0b6885e33df3aec333499f404aadecde1939dd4e
minhhdlum <tueminhlengoc@gmail.com>
2024-03-12 21:34:56 +0700

    First commit

--- DAL/Properties/AssemblyInfo.cs
+++ DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BAL")]
+[assembly: AssemblyTitle("DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BAL")]
+[assembly: AssemblyProduct("DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4cc5cb3-ea72-455b-ba22-a6281c1aa6af")]
+[assembly: Guid("5e454cde-456b-474c-a583-985cf5cf2e86")]
 
 // Version information for an assembly consists of the following four values:
 //
